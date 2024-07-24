6426e7327f19332cc0769616f43fb1a212c7b4c1
22f3001275 <22f3001275@ds.study.iitm.ac.in>
2024-07-24 22:56:54 +0530

    publish

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAKBORqvwwoVPavQjWYRBsIiKsPpnAzMDI",
-  authDomain: "portfolio-3d62f.firebaseapp.com",
-  projectId: "portfolio-3d62f",
-  storageBucket: "portfolio-3d62f.appspot.com",
-  messagingSenderId: "174398431968",
-  appId: "1:174398431968:web:ba76399a67746f87776685",
-  measurementId: "G-J9L99MMQC0"
+	apiKey: 'AIzaSyCX7QijWxe6SJfCo8sjZHPC5oBYXNlhq0I',
+	authDomain: 'portfolio-45e33.firebaseapp.com',
+	projectId: 'portfolio-45e33',
+	storageBucket: 'portfolio-45e33.appspot.com',
+	messagingSenderId: '811719981779',
+	appId: '1:811719981779:web:6e87f30c78a39aa2ae7226',
+	measurementId: 'G-2LGD5KKFJ4',
 };
 
 const app = initializeApp(firebaseConfig);
--- src/User.tsx
+++ src/User.tsx
@@ -1,74 +1,59 @@
 import { IconBrandGithub, IconBrandInstagram, IconBrandLeetcode, IconBrandLinkedin, IconBrandYoutube } from "@tabler/icons-react";
 const Info = {
-    name: "Chandrabhan Maurya",
-    stack: ["Software Engineer", "Full Stack Developer", "Competitive Programmer", "Freelancer", "Open Source Contributor"],
+    name: "Suyash Mishra",
+    stack: ["Software Engineer","Machine Learning Engineer", "Full Stack Developer", "Freelancer"],
     bio: "I'm a passionate software engineer specialized in crafting efficient and scalable solutions, deliver top-notch web applications, and provide freelance services that exceed client expectations. Let's connect and create something amazing together!"
 }
 
 
 
 const ProjectInfo = [
-    {
-        title: "Ekart E-comm App",
-        desc: "Ekart is a modern, fully responsive e-commerce platform built using React, TailwindCSS, Heroicons, and HeadlessUI, offering a seamless shopping experience across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Ekart combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
-        image: "Ekart.png",
-        live: true,
-        technologies: ["React", "Tailwind", "Redux", "Heroicons", "HeadlessUI"],
-        link: "https://code-mars.github.io/Ekart/",
-        github: "https://github.com/Code-Mars/Ekart/"
-    },
-    {
-        title: "Facebook Clone",
-        desc: "Facebook Clone is a full-stack web application that replicates the core features of Facebook, including user authentication, posting, and commenting. The front end is built with React, TailwinCSS, Tabler-Icons, Mantine and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, post updates, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Facebook Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Facebook.png",
-        live: false,
-        technologies: ["React", "Springboot", "MongoDB", "Tailwind", "Tabler-Icons", "Firebase"],
-        link: "https://github.com/Code-Mars/Facebook-Clone",
-        github: "https://github.com/Code-Mars/Facebook-Clone"
-    },
-    {
-        title: "Spotify Clone",
-        desc: "Spotify Clone is a full-stack web application that replicates the core features of Spotify, including user authentication, music playback, and playlist creation. The front end is built with React, TailwindCSS, Heroicons, and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, search for songs, and create playlists. The app features real-time updates, so users can see new songs as they are added. Spotify Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Spotify.png",
-        live: false,
-        technologies: ["React", "Springboot", "Tailwind", "Tabler-Icons", "MongoDB", "Firebase"],
-        link: "https://github.com/Code-Mars/Spotify-Clone",
-        github: "https://github.com/Code-Mars/Spotify-Clone"
-    },
-    {
-        title: "Travel Tracker",
-        desc: "Travel Tracker is a full-stack web application that allows users to track their travel experiences and share them with others. The front end is built with React, Bootstrap, while the back end uses Node.js, Express, and PostgresSQL. Users can create an account, log in, add new trips, and view their past trips on a map. The app features a clean, modern design with interactive maps and a user-friendly interface. Travel Tracker combines functionality with style, making it easy and enjoyable to record and share travel memories.",
-        image: "Travel.png",
-        live: false,
-        technologies: ["React", "Node.js", "PostgresSQL", "Express", "Bootstrap"],
-        link: "https://github.com/Code-Mars/Travel-Tracker",
-        github: "https://github.com/Code-Mars/Travel-Tracker"
-    },
-    {
-        title: "Instagram Clone",
-        desc: "Instagram Clone is a full-stack web application that replicates the core features of Instagram, including user authentication, image uploading, and commenting. The front end is built with React, TailwindCSS, and Heroicons, while the back end uses Firebase for user authentication and image storage. Users can create an account, log in, upload images, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Instagram Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Instagram.png",
-        live: false,
-        technologies: ["React", "Springboot", "Tailwind", "Heroicons", "Firebase", "MongoDB"],
-        link: "https://github.com/Code-Mars/Instagram-Clone",
-        github: "https://github.com/Code-Mars/Instagram-Clone"
-    },
-    {
-        title: "CodeX Code Editor",
-        desc: "CodeX is a modern, fully responsive code editor built using React, TailwindCSS, and Ace Editor, offering a seamless coding experience across all devices. It features syntax highlighting for popular programming languages, including HTML, CSS, JavaScript, Python, and Java. Users can create, edit, and save code files, with the option to download them as text files. CodeX combines robust functionality with a sleek, intuitive design, making coding effortless and enjoyable.",
-        image: "CodeX.png",
-        live: false,
-        technologies: ["React", "Tailwind", "Ace Editor"],
-        link: "https://github.com/Code-Mars/CodeX",
-        github: "https://github.com/Code-Mars/CodeX"
-    }
-]
+	{
+		title: 'Code N Grow',
+		desc: 'A full-stack coding platform with login system, where users solve coding problems and run their code online.',
+		image: 'https://suyashmishra.com/img/code-n-grow.png',
+		live: true,
+		technologies: [
+			'NextJS',
+			'TypeScript',
+			'Tailwind CSS',
+			'Firebase',
+			'Vercel',
+		],
+		link: 'https://code-n-grow.vercel.app',
+		github: '#',
+	},
+	{
+		title: 'Personal Portfolio Website',
+		desc: 'A frontEnd project with Dark mode made using Framer Motion for UI animations, deployed on vercel. ',
+		image: 'https://suyashmishra.com/img/portfolio.png',
+		live: true,
+		technologies: [
+			'NextJS',
+			'TypeScript',
+			'Tailwind CSS',
+			'Framer Motion',
+			'Vercel',
+		],
+		link: 'https://mishrasuyash.vercel.app/',
+		github: 'https://github.com/mishra-suyash/portfolio-nextjs',
+	},
+	{
+		title: 'ENGIFY ECom Website',
+		desc: 'A Official StoreFront with Catalog and Live Payments using Wordpress and Custom made plugin for Payment Gateways integrated with Woo Commerce that can send confirmation mail to Users.',
+		image: 'https://suyashmishra.com/img/engify.png',
+		live: true,
+		technologies: ['Wordpress', 'PHP', 'Woo Commerce', 'Payment APIs'],
+		link: 'https://engify.in',
+		github: '#',
+	},
+];
 
 
 const SkillInfo = [
     {
         title: "Frontend",
-        skills: ["HTML", "CSS", "SASS", "JavaScript", "React JS", "Angular", "Redux", "Tailwind CSS", "GSAP", "Material UI", "Bootstrap"
+        skills: ["HTML", "CSS", "SASS", "JavaScript", "React JS", "Redux", "Tailwind CSS",  "Material UI", "Bootstrap"
         ]
     },
     {
@@ -77,7 +62,7 @@ const SkillInfo = [
     },
     {
         title: "Languages",
-        skills: ["C", "C++", "Java", "JavaScript", "TypeScript"]
+        skills: ["C", "C++", "Java", "JavaScript", "TypeScript", "Python"]
     },
     {
         title: "Tools",
@@ -85,49 +70,49 @@ const SkillInfo = [
     }
 ]
 const socialLinks = [
-    { link: "https://github.com/Code-Mars", icon: IconBrandGithub },
-    { link: "https://www.linkedin.com/in/chandrabhan-maurya", icon: IconBrandLinkedin },
-    { link: "https://www.instagram.com/code.marshal_", icon: IconBrandInstagram }, 
-    { link: "https://www.youtube.com/channel/UC1ki6jaFvFiH_E79b9FYptw", icon: IconBrandYoutube },
-    { link: "https://www.leetcode.com/u/CodeMars", icon: IconBrandLeetcode }
+	{ link: 'https://github.com/mishra-suyash', icon: IconBrandGithub },
+	{
+		link: 'https://www.linkedin.com/in/onlysuyash/',
+		icon: IconBrandLinkedin,
+	},
+	{ link: 'https://leetcode.com/u/mishrasuyash/', icon: IconBrandLeetcode },
 ];
 
 
 const ExperienceInfo = [
-    {
-        role: "Specialist Programmer",
-        company: "Infosys",
-        date: "Oct 2023 - Present",
-        desc: "I led software development with Spring Boot, React, and Angular, creating scalable microservices and interfaces. I optimized performance by integrating frontend and backend, and enhanced security with RESTful APIs. Collaborating in agile teams, I fostered innovation and efficiency for top-quality solutions.",
-        skills: ["Springboot", "React JS", "Angular", "Node JS", "MySQL", "MongoDB", "Microservices"]
-    },
-    {
-        role: "System Engineer",
-        company: "Infosys",
-        date: "Nov 2022 - Sep 2023",
-        desc: " I leveraged my skills in Java, Selenium, automation testing, and MySQL to design and implement automated testing frameworks, ensuring robust software validation. I optimized database performance and reliability, and collaborated with cross-functional teams to enhance system operations and ensure seamless integration.",
-        skills: ["Java", "MySQL", "Hibernate", "Selenium", "Jenkins", "JIRA", "Automation Testing"]
-    }
-]
+	{
+		role: 'Campus Ambassador',
+		company: 'GEEKSFORGEEKS',
+		date: 'Aug 2023 - May 2024',
+		desc: 'Organised seminars on Road-maps,Sessions covering Web Development and Hackathons',
+		skills: ['React JS', 'Angular', 'Node JS', 'MySQL', 'MongoDB'],
+	},
+	{
+		role: 'Wordpress/PHP Developer',
+		company: 'ENGIFY',
+		date: 'Nov 2022 - Sep 2023',
+		desc: ' Developed E-Commerce store using WooCommerce and Integrated Payment Gateways-Razorpay, PayU using custom made plugin written in PHP',
+		skills: [
+			'Wordpress',
+			'PHP',
+			'Woo-Commerce',
+			'MySQL',
+		],
+	},
+];
 const Slugs = [
     "typescript",
-    "spring",
     "javascript",
-    "dart",
     "java",
     "react",
-    "angular",
     "flutter",
-    "android",
     "html5",
     "css3",
-    "springboot",
     "mongodb",
     "selenium",
     "nodedotjs",
     "express",
     "nextdotjs",
-    "prisma",
     "mysql",
     "amazonaws",
     "postgresql",
@@ -139,12 +124,9 @@ const Slugs = [
     "cypress",
     "docker",
     "git",
-    "jira",
     "github",
     "gitlab",
     "visualstudiocode",
     "androidstudio",
-    "sonarqube",
-    "figma",
 ];
 export { Info, ProjectInfo,socialLinks, SkillInfo, ExperienceInfo, Slugs };
